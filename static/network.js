7a9286c5247bf6aafcee9234fbaa1823a173a565
Irwin <irwinngo@gmail.com>
2022-10-15 10:52:02 -0400

    Added three.js

---
+++ static/network.js
@@ -0,0 +1,30 @@
+/*import * as THREE from 'https://cdn.skypack.dev/three@0.132.2';
+console.log("Test");
+const scene = new THREE.scene();
+
+const camera = new THREE.perspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+renderer.render (scene, camera);
+
+const geometry = new THREE.TorusGeometry(10, 3, 6, 100);
+const material = new THREE.MeshBasicMaterial( { color = 0xFF6347, wireframe = true});
+const torus = new THREE.Mesh(geometry, material);
+
+scene.add(torus);
+
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+*/
+
+var changeColor = document.querySelector('.test');
+changeColor.style.backgroundColor="#00ac00";
