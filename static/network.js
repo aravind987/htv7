d0af90eaca6ae8d76eae4bef07ce5238e1fcd50d
Irwin <irwinngo@gmail.com>
2022-10-15 10:52:11 -0400

    Added three.js

--- static/network.js
+++ static/network.js
@@ -1,11 +1,15 @@
-/*import * as THREE from 'https://cdn.skypack.dev/three@0.132.2';
+import * as THREE from 'https://cdn.skypack.dev/three@0.132.2';
+import { OrbitControls } from 'https://cdn.skypack.dev/three@0.132.2/examples/jsm/controls/OrbitControls.js';
 console.log("Test");
-const scene = new THREE.scene();
+const scene = new THREE.Scene();
+scene.background = new THREE.Color( 0xffffff );
 
-const camera = new THREE.perspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector('#bg'),
+    alpha: true
 });
 
 renderer.setPixelRatio( window.devicePixelRatio );
@@ -14,17 +18,26 @@ camera.position.setZ(30);
 
 renderer.render (scene, camera);
 
-const geometry = new THREE.TorusGeometry(10, 3, 6, 100);
-const material = new THREE.MeshBasicMaterial( { color = 0xFF6347, wireframe = true});
-const torus = new THREE.Mesh(geometry, material);
+const geometry = new THREE.CircleGeometry(5, 32);
+const material = new THREE.MeshBasicMaterial( { color: 0xFF6347, wireframe: false});
+const circle = new THREE.Mesh(geometry, material);
 
-scene.add(torus);
+scene.add(circle);
+
+const controls = new OrbitControls( camera, renderer.domElement );
+
+controls.enableRotate = false;
+
+controls.mouseButtons = {
+    LEFT: THREE.MOUSE.PAN,
+    MIDDLE: THREE.MOUSE.DOLLY,
+    RIGHT: THREE.MOUSE.ROTATE
+}
+
 
 function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
 }
-*/
 
-var changeColor = document.querySelector('.test');
-changeColor.style.backgroundColor="#00ac00";
+animate();
