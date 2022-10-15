948283bd7fc1cecd007379317e609d191bbc4564
celinekwan <celine.kwan2017@gmail.com>
2022-10-15 17:38:56 -0400

    edits

--- main.py
+++ main.py
@@ -3,25 +3,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
+#home page
 @app.route('/')
-# def homePage():
-#     return render_template('homepage.html', title="Home")
+def homePage():
+    return render_template('index.html', title="Home")
 
-# show network page
-# @app.route('/network')
-# def networkPage():
-#     return render_template('network.html', title="Network")
-
-#database page
+#add connection page
 @app.route('/add-connection')
 def addConnectionPage():
     return render_template('add-connection.html', title="Add Connection")
 
+# network page
+@app.route('/network')
+def networkPage():
+    return render_template('network.html', title="Network")
+
 #database page
-# @app.route('/database')
-# def databasePage():
-#     return render_template('database.html', title="Database")
+@app.route('/database')
+def databasePage():
+    return render_template('database.html', title="Database")
 
 if __name__ == '__main__':
     app.run()
