4197bd1a5639e9e20b6d1d38d78ef9c89c67a732
celinekwan <celine.kwan2017@gmail.com>
2022-10-15 12:58:58 -0400

    pull html pages from main.py

--- main.py
+++ main.py
@@ -5,9 +5,23 @@ app = Flask(__name__)
 
 
 @app.route('/')
+# def homePage():
+#     return render_template('homepage.html', title="Home")
+
+# show network page
 @app.route('/network')
 def networkPage():
     return render_template('network.html', title="Network")
+
+#database page
+@app.route('/add-connection')
+def databasePage():
+    return render_template('add-connection.html', title="Add Connection")
+
+#database page
+@app.route('/database')
+def databasePage():
+    return render_template('database.html', title="Database")
 
 if __name__ == '__main__':
     app.run()
