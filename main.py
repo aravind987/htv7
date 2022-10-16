a7f1b5d5012ee8ebe5d47a8294633d58d6e77885
aravind987 <50051060+aravind987@users.noreply.github.com>
2022-10-16 02:18:48 -0400

    I DID ITTTTTTT LESGOOOO

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from operator import methodcaller
+from xml.dom.pulldom import END_DOCUMENT
 from relationshipList import relationshipList
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request, redirect
 import json
 from flask_cors import CORS
 
@@ -10,6 +12,30 @@ CORS(app)
 @app.route('/')
 def homePage():
     return render_template('index.html', title="Home")
+
+#handle form
+@app.route('/handleForm',methods=['POST'])
+def handleForm():
+    firstName = request.form['firstName']
+    lastName = request.form['lastName']
+    fullName = firstName + lastName
+    date = request.form['dateMet']
+    company = request.form['company']
+    jobTitle = request.form['jobTitle']
+    industry = request.form['industry']
+    education = request.form['education']
+    email = request.form['inputEmail']
+    linkedin = request.form['linkedin']
+    phoneNum = request.form['phoneNumber']
+    otherNotes = request.form['otherNotes']
+
+    file = open('./data/relationshipData.json')
+    relationshipDict = (json.load(file))["relationships"]
+
+    relationshipDict[fullName] = [firstName,lastName,date,[phoneNum,email],company,industry,jobTitle,education,otherNotes]
+    file = open('./data/relationshipData.json',"w")
+    file.write(json.dumps(relationshipDict))
+    return redirect ('/')
 
 # show network page
 @app.route('/network')
